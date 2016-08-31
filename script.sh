0dddc4b353cf5a36ad40e111f42d7254ce094992
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-08-31 15:08:45 -0500

    Case 5

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
-PWD=`pwd`
-./node_modules/.bin/ngc -p $PWD/config/build/tsconfig.json
+#PWD=`pwd`
+#./node_modules/.bin/ngc -p $PWD/config/build/tsconfig.json
+./node_modules/.bin/ngc -p ./config/build/tsconfig.json
